12c2a1115b17aa8a13ea80960c85ddee42eb8fba
zazu7765 <ohmyzazu@gmail.com>
2022-10-20 16:56:12 -0400

    add serial communication

--- main.cpp
+++ main.cpp
@@ -9,31 +9,47 @@ using namespace std::chrono_literals;
 int main(int argc, char* argv[]) {
     system::error_code ec;
     asio::io_context ioContext;
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address(argv[1], ec), 65432);
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address(argv[1], ec), std::atoi(argv[2]));
     asio::ip::tcp::socket socket(ioContext);
-    socket.connect(endpoint, ec);
-    if (!ec) { std::cout << "connected" << std::endl; }
-    else { std::cout << "failed" << std::endl; };
-    if (socket.is_open()) {
-        std::string request = "testing testing 123";
-        socket.write_some(asio::buffer(request.data(), request.size()), ec);
-        for (;;) {
-            std::this_thread::sleep_for(1ms);
-            size_t bytes = socket.available();
-            socket.wait(socket.wait_read);
-            if (bytes > 0) {
-                std::vector<char> vBuffer(bytes);
-                socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-                std::string message{vBuffer.begin(), vBuffer.end()};
-                std::cout<<message<<std::endl;
-                if(message=="quit"){
-                    socket.close();
+    try {
+        asio::serial_port serial(ioContext);
+        serial.open("/dev/ttyACM0", ec);
+        serial.set_option(asio::serial_port_base::baud_rate(115200));
+        socket.connect(endpoint, ec);
+        if (!ec) std::cout << "SOCKET CONNECTED" << std::endl;
+        else std::cout << "failed" << std::endl;
+        auto socketopen = socket.is_open();
+        if (socketopen) {
+            std::cout<<"POTATO"<<std::endl;
+            std::string request = "Status: READY";
+            std::cout<<request<<std::endl;
+            socket.write_some(asio::buffer(request.data(), request.size()), ec);
+            for (;;) {
+                std::this_thread::sleep_for(1ms);
+                size_t bytes = socket.available();
+                socket.wait(socket.wait_read);
+                if (bytes > 0) {
+                    std::vector<char> vBuffer(bytes);
+                    std::vector<char> serialBuffer(bytes);
+                    socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+                    std::string message{vBuffer.begin(), vBuffer.end()};
+                    asio::write(serial, asio::buffer(message));
+                    std::cout << "from socket: "<< message << std::endl;
+                    serial.read_some(asio::buffer(serialBuffer.data(), serialBuffer.size()), ec);
+                    std::string serialMessage{serialBuffer.begin(), serialBuffer.end()};
+                    std::cout<< "from serial: "<< serialMessage << std::endl;
+//                socket.write_some(asio::buffer(vBuffer));
+                    if (message == "quit"){
+                        socket.close(ec);
+                        serial.close(ec);
+                        return 0;
+                    }
                 }
-                //for (auto c: vBuffer) {
-                  //  std::cout << c;
-               // }
-                //std::cout << "" << std::endl;
             }
         }
+        std::cout<<"PAST IF STATEMENT"<<std::endl;
+    }
+    catch(system::error_code& ec){
+
     }
 }
