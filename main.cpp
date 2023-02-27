c5859dac7cec08aabb5246f2bac6722bfb1c3cad
zazu7765 <ohmyzazu@gmail.com>
2023-02-27 11:34:26 -0500

    basic modbus testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include <modbus/modbus-tcp.h>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    modbus_t *mb;
+    uint16_t tab_reg[32];
+    mb = modbus_new_tcp("",502);
+    modbus_connect(mb);
+    modbus_read_registers(mb,16386, 5, tab_reg);
+    modbus_close(mb);
+    modbus_free(mb);
     return 0;
 }
