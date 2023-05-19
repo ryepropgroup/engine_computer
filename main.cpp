e70abae325010bbb6b55313d25d9283c67665d9d
zazu7765 <ohmyzazu@gmail.com>
2023-05-19 13:19:24 -0400

    signals/slots testing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
+#include <iostream>
 #include <modbus/modbus-tcp.h>
+#include <boost/signals2.hpp>
+#include <thread>
+
+struct ljSetup {
+    double p1 = 0, p2 = 0, p3 = 0;
+    const char *p1Name = {"AIN0"};
+    const char *p2Name = {"AIN2"};
+    const char *p3Name = {"AIN4"};
+    std::array<const char *, 2> p1Params = {"AIN0_RANGE", "AIN0_NEGATIVE_CH"};
+    std::array<const char *, 2> p2Params = {"AIN2_RANGE", "AIN2_NEGATIVE_CH"};
+    std::array<const char *, 2> p4Params = {"AIN4_RANGE", "AIN4_NEGATIVE_CH"};
+    std::array<double, 2> p1Vals = {0.1, 1.0};
+    std::array<double, 2> p2Vals = {0.1, 3.0};
+    std::array<double, 2> p4Vals = {0.1, 5.0};
+};
+
+void print_p1Name(ljSetup lj) {
+    std::cout << lj.p1Name << std::endl;
+}
+
+void print_p2Name(ljSetup lj) {
+    std::cout << lj.p2Name << std::endl;
+}
+
+void run_signal(std::shared_ptr<boost::signals2::signal<void(ljSetup)>> sig){
+    ljSetup lj = *new ljSetup();
+    for(int i = 0 ; i < 100; i ++)
+        (*sig)(lj);
+}
 int main() {
-    modbus_t *mb;
-    uint16_t tab_reg[32];
-    mb = modbus_new_tcp("",502);
-    modbus_connect(mb);
-    modbus_read_registers(mb,16386, 5, tab_reg);
-    modbus_close(mb);
-    modbus_free(mb);
+    // modbus_t *mb;
+    // uint16_t tab_reg[32];
+    // mb = modbus_new_tcp("", 502);
+    // modbus_connect(mb);
+    // modbus_read_registers(mb, 16386, 5, tab_reg);
+    // modbus_close(mb);
+    // modbus_free(mb);
+
+    std::shared_ptr<boost::signals2::signal<void(ljSetup)>> testSig(new boost::signals2::signal<void(ljSetup)>);
+    testSig->connect(&print_p1Name);
+    testSig->connect(&print_p2Name);
+    std::jthread one(run_signal, testSig);
     return 0;
 }
