ef1267fae3ecfaebd9b645f4d4a8b2ccc7df5718
zazu7765 <ohmyzazu@gmail.com>
2023-05-23 20:19:37 -0400

    add json library + split into structs

--- main.cpp
+++ main.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
-//#include <modbus/modbus-tcp.h>
+#include <modbus/modbus-tcp.h>
 #include <boost/signals2.hpp>
 #include <thread>
 #include "LJM_Utilities.h"
-
+#include "json.hpp"
+using json = nlohmann::json;
 using namespace std::chrono_literals;
-struct ljSetup {
-    double p1 = 0, p2 = 0, p3 = 0;
-    const char *p1Name = {"AIN0"};
-    const char *p2Name = {"AIN2"};
-    const char *p3Name = {"AIN4"};
-    std::array<const char *, 2> p1Params = {"AIN0_RANGE", "AIN0_NEGATIVE_CH"};
-    std::array<const char *, 2> p2Params = {"AIN2_RANGE", "AIN2_NEGATIVE_CH"};
-    std::array<const char *, 2> p4Params = {"AIN4_RANGE", "AIN4_NEGATIVE_CH"};
-    std::array<double, 2> p1Vals = {0.1, 1.0};
-    std::array<double, 2> p2Vals = {0.1, 3.0};
-    std::array<double, 2> p4Vals = {0.1, 5.0};
+
+struct Sensor {
+    Sensor(std::string name, std::vector<std::string> params, const std::vector<double>& settings) {
+        this->name = std::move(name);
+        this->params = std::move(params);
+        this->settings = settings;
+    }
+    std::string name;
+    std::vector<std::string> params;
+    std::vector<double> settings;
 };
 
-struct sd {
-    std::map<std::string, double> sensors{};
+struct LJSensors{
+    double p1val = 0, p2val = 0, p3val = 0;
+    Sensor *p1 = new Sensor(std::string("AIN0"), std::vector<std::string>{"AIN0_range","AIN0_NEGATIVE_CH"}, std::vector<double>{0.1, 1.0});
+    Sensor *p2 = new Sensor(std::string("AIN2"), std::vector<std::string>{"AIN2_range","AIN2_NEGATIVE_CH"}, std::vector<double>{0.1, 3.0});
+    Sensor *p4 = new Sensor(std::string("AIN4"), std::vector<std::string>{"AIN4_range","AIN4_NEGATIVE_CH"}, std::vector<double>{0.1, 5.0});
 };
 
-void print_p1Name(ljSetup lj) {
-    std::cout << lj.p1Name << std::endl;
+struct State{
+    bool launched = false;
+    LJSensors lj;
+    std::map<std::string, bool> valves;
+};
+void print_p1Name(LJSensors lj) {
+    std::cout << lj.p1->name << std::endl;
 }
 
-void print_p2Name(ljSetup lj) {
-    std::cout << lj.p2Name << std::endl;
+void print_p2Name(LJSensors lj) {
+    std::cout << lj.p2->name << std::endl;
 }
 
-void run_signal(const boost::signals2::signal<void(ljSetup)> *sig, const std::stop_token *t) {
-    ljSetup lj = *new ljSetup();
+void run_signal(const boost::signals2::signal<void(LJSensors)> *sig, const std::stop_token *t) {
+    LJSensors lj = *new LJSensors();
     while (!t->stop_requested()) {
         (*sig)(lj);
     }
@@ -44,16 +52,16 @@ void stop_func(const std::stop_source *ss) {
 }
 
 int main() {
-    // modbus_t *mb;
-    // uint16_t tab_reg[32];
-    // mb = modbus_new_tcp("", 502);
-    // modbus_connect(mb);
-    // modbus_read_registers(mb, 16386, 5, tab_reg);
-    // modbus_close(mb);
-    // modbus_free(mb);
+//     modbus_t *mb;
+//     uint16_t tab_reg[32];
+//     mb = modbus_new_tcp("", 502);
+//     modbus_connect(mb);
+//     modbus_read_registers(mb, 16386, 5, tab_reg);
+//     modbus_close(mb);
+//     modbus_free(mb);
     std::stop_source test;
     std::stop_token token = test.get_token();
-    boost::signals2::signal<void(ljSetup)> testSig;
+    boost::signals2::signal<void(LJSensors)> testSig;
     testSig.connect(&print_p1Name);
     testSig.connect(&print_p2Name);
     std::jthread one(run_signal, &testSig, &token);
